86757e6d4844634c9bb7a05c2b15587f6f47e7a5
Kenneth Radunz <kenneth.radunz@gmail.com>
2017-03-05 21:23:59 +0100

    add example project

--- javafxcore/src/test/kotlin/ch/tkoc/fx/component/LoginForm.kt
+++
@@ -1,6 +0,0 @@
-package ch.tkoc.fx.component
-
-import javafx.scene.layout.FlowPane
-
-
-class LoginForm : FlowPane()
--- javafxcore/src/test/kotlin/ch/tkoc/fx/component/LoginView.kt
+++
@@ -1,7 +0,0 @@
-package ch.tkoc.fx.component
-
-import ch.tkoc.fx.View
-import javafx.scene.layout.FlowPane
-
-
-class LoginView : View<FlowPane>()
