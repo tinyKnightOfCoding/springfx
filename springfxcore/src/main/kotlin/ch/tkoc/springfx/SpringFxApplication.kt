1902e0a6ac08860fb8b8e1463f5a3db185d62f1b
Kenneth Radunz <kenneth.radunz@gmail.com>
2017-03-05 21:42:54 +0100

    change JavaFxAwareAppContext to take config classes instead of another appContext

--- springfxcore/src/main/kotlin/ch/tkoc/springfx/SpringFxApplication.kt
+++ springfxcore/src/main/kotlin/ch/tkoc/springfx/SpringFxApplication.kt
@@ -2,21 +2,22 @@ package ch.tkoc.springfx
 
 import javafx.application.Application
 import javafx.stage.Stage
-import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 
-fun launch(appContext: AnnotationConfigApplicationContext) {
-    if(customAppContext != null) {
-        customAppContext = appContext
+fun launch(configurationClass: Class<*>) {
+    if(customConfigClass == null) {
+        customConfigClass = configurationClass
         Application.launch(SpringFxApplication::class.java)
     }
 }
 
-internal var customAppContext : AnnotationConfigApplicationContext? = null
+internal var customConfigClass : Class<*>? = null
 
 class SpringFxApplication : Application() {
 
-    override fun start(primaryStage: Stage) : Unit = customAppContext!!.let {
-        JavaFxAwareApplicationContext(primaryStage, it)
+    override fun start(primaryStage: Stage) : Unit = customConfigClass!!.let {
+        JavaFxAwareApplicationContext(it).apply {
+            beanDefinitionNames.forEach { println("- $it") }
+        }
     }
 }
--- springfxexample/src/main/kotlin/ch/tkoc/springfx/example/ExampleConfiguration.kt
+++ springfxexample/src/main/kotlin/ch/tkoc/springfx/example/ExampleConfiguration.kt
@@ -1,12 +1,11 @@
 package ch.tkoc.springfx.example
 
 import ch.tkoc.springfx.launch
-import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class ExampleConfiguration
 
 fun main(vararg args: String) {
-    launch(AnnotationConfigApplicationContext(ExampleConfiguration::class.java))
+    launch(ExampleConfiguration::class.java)
 }
